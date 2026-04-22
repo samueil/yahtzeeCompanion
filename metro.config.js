bf99a0d8879e9ef07a3f30db322fa82553154666
Daboss-ca <chris.arnuco12@gmail.com>
2026-04-22 15:27:50 +0800

    fix linting errors

--- metro.config.js
+++ metro.config.js
@@ -1,6 +1,6 @@
+const path = require('path');
 const { getDefaultConfig } = require('expo/metro-config');
 const { withNativeWind } = require('nativewind/metro');
-const path = require('path');
 
 const config = getDefaultConfig(__dirname);
 
